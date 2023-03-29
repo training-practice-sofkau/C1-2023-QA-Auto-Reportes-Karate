c8406e989af6cc657f817517ea4e7e41f6d73fe6
juan david cardona velasquez <j.cardona1@utp.edu.co>
2023-03-28 23:17:30 -0500

    Se agrega el primer servicio POST (crear una tarea)

---
+++ ApiRestSerenity/src/main/java/com/sofkau/models/Tarea.java
@@ -0,0 +1,42 @@
+package com.sofkau.models;
+
+import io.cucumber.java.sl.In;
+
+public class Tarea {
+    private String userId;
+    private Integer id;
+    private String title;
+    private String completed;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(String completed) {
+        this.completed = completed;
+    }
+}
---
+++ ApiRestSerenity/src/test/resources/features/newtarea.feature
@@ -0,0 +1,14 @@
+Feature: Create Todos on the Page
+  AS  JsonPlaceHolder user
+  I WANT TO
+  create a todo on the site
+  in order to have a list of todos
+
+  @Register
+  Scenario Outline: Succesfull Task Creation
+    Given I have the service endpoint
+    When the user send a POST request with the task data <userId>,<id>,<title>,<completed>
+    Then the user sees a successful status code and the body of the response.
+    Examples:
+      | userId | id  | title          | completed |
+      | "1"    | 201 | "Hacer oficio" | "false"   |
